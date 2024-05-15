851aa32178a1368bd557949b7a0dcae9d4057e08
Habibur Rahman <hr13338@gmail.com>
2024-05-15 15:12:06 +0600

    This the primary setup for the todo project with spring backend

---
+++ src/test/java/com/todoList/TodoServer/TodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoList.TodoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
